94653208bfe20be68f0f4a9d1c35ac6fd7908181
BenBowring <benjamin.bowring@gmail.com>
2023-03-01 22:34:36 +0000

    All that remains is custom sidebar

--- stream_file.py
+++ stream_file.py
@@ -5,25 +5,20 @@ import pandas as pd
 import numpy as np
 import plotly.graph_objects as go
 
+
+# Boring setup stuff for url slug and page width
 st.set_page_config(layout="wide")
 url = "https://api.opentopodata.org/v1/srtm90m"
-colorscale = [[0, 'black'], [1, 'black']]
 
-colors = {
-    'background': '#000000',
-    'text': '#FFFFFF'
-}
-
-# Chunk the request so url doesnt bounce us
+# Define a function that chunks the request so url doesnt bounce us
 def chunks(lst, n):
     """Yield successive n-sized chunks from lst."""
     for i in range(0, len(lst), n):
         yield lst[i:i + n]
 
-# URL pull, cached so we don't bombard
 
-@st.cache_data
-def get_grid(lat_co, lon_co, size, granular):
+# Function that creates a grid around the co-ord space and gets elevation at each point
+def get_grid(lat_co, lon_co, granular, size):
     
     mod = granular // 2
     size_norm = size / 110.574
@@ -55,26 +50,25 @@ def get_grid(lat_co, lon_co, size, granular):
 
     return data
 
-# Figure Function
-
-@st.cache_data
-def update_chart(input1, input2, granularity, size = 2, labels = True, line_smooth = 1):
+# Contour plot function
+def update_chart(input1, input2, granularity, size, elev_labels, city_label, line_smooth = 1):
     
-    z_data = get_grid(input1, input2, size, granularity)
+    z_data = get_grid(input1, input2, granularity, size)
     
     fig = go.Figure(data =
         go.Contour(
+            hoverinfo='none',
             z = z_data.values,
             line_smoothing=line_smooth,
-            line_color = 'slategrey',
+            line_color = st.session_state.ForeCol,
             line_width = 1.25,
             showscale=False,
-            colorscale = colorscale,
+            colorscale = [[0, st.session_state.BackCol], [1, st.session_state.BackCol]],
             contours=dict(
-                showlabels = labels, # show labels on contours
+                showlabels = elev_labels, # show labels on contours
                 labelfont = dict( # label font properties
                     size = 14,
-                    color = 'slategrey',
+                    color = st.session_state.ForeCol,
                     family = 'Droid Serif'
                 )
             )
@@ -86,7 +80,7 @@ def update_chart(input1, input2, granularity, size = 2, labels = True, line_smoo
         yaxis={'visible': False,'fixedrange':True},
         xaxis={'visible': False,'fixedrange':True},
         height = 800,
-        #width = 2400,
+        #width = width,
         margin=dict(
             l=0,
             r=0,
@@ -98,84 +92,106 @@ def update_chart(input1, input2, granularity, size = 2, labels = True, line_smoo
 
     )
 
-    fig.add_annotation(text=city,
-                  xref="paper", yref="paper", textangle=-90,
-                  x=0.95, y=0.95, showarrow=False)
-    
-    fig.layout.font.family = "Droid Serif"
-    fig.layout.font.size = 32
-    fig.layout.font.color = 'slategrey'
+    if city_label:
+
+        fig.add_annotation(text=city,
+                    xref="paper", yref="paper", textangle=-90,
+                    x=0.95, y=0.95, showarrow=False)
+        
+        fig.layout.font.family = "Droid Serif"
+        fig.layout.font.size = 32
+        fig.layout.font.color = st.session_state.TextCol
     
     return fig
 
-
+# Initiate sidebar with text and the like
 st.sidebar.title("Plot the contour map for a certain city or custom co-ords :globe_with_meridians:")
-st.sidebar.write(
-    "Adjust the scale & colours of map and adjust to a given ratio for wallpapers downloads if required!"
+st.sidebar.header(
+    "_Adjust the scale & colours of map and hover over to download!_"
 )
 st.sidebar.write(
-    "_Source code on my [GitHub](https://github.com/BenBowring/Contours)._"
+    "_Source code on [GitHub](https://github.com/BenBowring/Contours)._"
 )
 
 st.sidebar.markdown('#')
-st.sidebar.markdown('#')
 
-# User selection of generation inputs
-
-granny_dict = {'S': 5, 'M': 9, 'L': 15}
-size_dict = {'S': 2, 'M': 3, 'L': 5}
+# Define dictionaries that hold keys in selections and associated values
+scale_dict = {'S': [5,2], 'M': [9,3], 'L': [15,5]}
 label_dict = {'Yes': True, 'Nah': False}
 city_dict = {'Belfast': [54.5973, -5.9301], 'Edinburgh': [55.9533, -3.1883], 
              'New York': [40.7128, -74.0060], 'Rio': [-22.9068, -43.1729]}
+image_dict = {'800x800': [800, 800], '1920x1080': [1920,1080]}
 
 
-
+# First selections for city and size of elevation grid
 city = st.sidebar.selectbox('Select City:', [x for x in city_dict.keys()])
-granularilty = st.sidebar.selectbox('Select Granularity:', ['S', 'M', 'L'])
-size = st.sidebar.selectbox('Select Scale:', ['S', 'M', 'L'])
+scale = st.sidebar.selectbox('Select Scale:', [x for x in scale_dict.keys()])
+
+custom_coords = st.sidebar.checkbox('Custom Co-Ordinates:', value = False, key = 'custom_coords')
+
+
+# st.sidebar.markdown('#')
+
+# with st.sidebar:
+
+#     size_col = st.columns([1,1], gap = 'small')
+        
+#     with st.form(key = 'DownloadVals'):
+
+#         with size_col[0]:
+            
+#             pic_size = st.selectbox('Select Image Size:', [x for x in image_dict.keys()], key = 'ImageSize')
+            
+# Define reset values that will be used in form state
+def resetcolVals():
+
+    st.session_state.Elevation = 'Nah'
+    st.session_state.Label = 'Yes'
+    st.session_state.BackCol = '#000000'
+    st.session_state.ForeCol = '#616375'
+    st.session_state.TextCol = '#616375'
 
 st.sidebar.markdown('#')
 
-#reset_button = st.button("Reset Chart Appearance") # Give button a variable name
-#if reset_button: # Make button a condition.
-#    start_capture()
-#    st.text("Captured Successfully")
+# Wrap in form to allow resetting of all adjustment values
+with st.form(key = 'AppVals'):
 
-st.sidebar.markdown('#')
+    with st.sidebar:
 
-side_col_1_1, side_col_1_2 = st.sidebar.columns([1,1], gap = 'large')
-with side_col_1_1:
-    line_smooth = st.slider('Contour Smoothness:', 0.0, 1.0, 1.0, step = 0.1)
+        st.write('Adjust Visuals:')
 
-with side_col_1_2:
-    show_labs = st.radio('Show Elevation Labels:', ['Nah', 'Yes'], horizontal=True)
+        side_col_1_1, side_col_1_2 = st.columns([1,1], gap = 'small')
 
-st.sidebar.markdown('#')
+        with side_col_1_1:
+            show_elev_labs = st.radio('Show Elevation:', ['Nah', 'Yes'], horizontal=True, key = 'Elevation')
 
-side_col_1, side_col_2, side_col_3 = st.sidebar.columns([1,1,1], gap = 'small')
-with side_col_1:
-    background_col = st.color_picker('Background', '#000000')
+        with side_col_1_2:
+            show_city_labs = st.radio('Show City:', ['Nah', 'Yes'], index = 1, horizontal=True, key = 'Label')
 
-with side_col_2:
-    contour_col = st.color_picker('Contours', '#616375')
-    
-with side_col_3:
-    text_col = st.color_picker('Text', '#616375')
+        side_col_1, side_col_2, side_col_3 = st.columns([1,1,1], gap = 'small')
 
-# Define dicts so we can pull associated values
+        with side_col_1:
+            background_col = st.color_picker('Background', '#000000', key = 'BackCol')
 
+        with side_col_2:
+            contour_col = st.color_picker('Contours', '#616375', key = 'ForeCol')
+            
+        with side_col_3:
+            text_col = st.color_picker('Text', '#616375', key = 'TextCol')
 
-
-# Select everything from the inputs on the page
+        reset_button = st.form_submit_button(label = 'Reset Visuals', on_click=resetcolVals)
 
 
+
+# Call figure chart with all selected variables
 fig = update_chart(city_dict.get(city)[0], city_dict.get(city)[1], 
-                   granny_dict.get(granularilty), 
-                   size_dict.get(size),
-                   label_dict.get(show_labs),
-                   line_smooth
+                   scale_dict.get(scale)[0], 
+                   scale_dict.get(scale)[1],
+                   label_dict.get(show_elev_labs),
+                   label_dict.get(show_city_labs)
                    )
 
 
 
+# Plot chart, noting container width var
 st.plotly_chart(fig, use_container_width=True)
